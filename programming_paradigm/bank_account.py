effb213f4b7b2010a15b01a116826778691e4816
Randa-mamdouh <randaranood95@gmail.com>
2025-06-15 14:52:46 +0300

    new files added

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,17 @@
+class BankAccount:
+    def __init__(self,balance=0):
+        self.account_balance=balance
+    def deposit(self,amount):
+        if amount>0:
+            self.account_balance+=amount
+        else:
+            print("amount must be positive")
+    def withdraw(self,amount):
+        if 0<amount>=self.account_balance:
+            self.account_balance-=amount
+            return True
+        else:
+            print("Insufficient funds or invalid amount.")
+            return False
+    def display_balance(self):
+        print(f"current balance : ${self.account_balance} ")
---
+++ programming_paradigm/test_simple_calculator.py
@@ -0,0 +1,39 @@
+import unittest
+from simple_calculator import SimpleCalculator
+
+class TestSimpleCalculator(unittest.TestCase):
+
+    def setUp(self):
+        """Set up the SimpleCalculator instance before each test."""
+        self.calc = SimpleCalculator()
+
+    def test_add(self):
+        self.assertEqual(self.calc.add(2, 3), 5)
+        self.assertEqual(self.calc.add(-1, 1), 0)
+        self.assertEqual(self.calc.add(-5, -3), -8)
+        self.assertEqual(self.calc.add(0, 0), 0)
+
+    def test_subtract(self):
+        self.assertEqual(self.calc.subtract(5, 3), 2)
+        self.assertEqual(self.calc.subtract(3, 5), -2)
+        self.assertEqual(self.calc.subtract(0, 0), 0)
+        self.assertEqual(self.calc.subtract(-3, -2), -1)
+
+    def test_multiply(self):
+        self.assertEqual(self.calc.multiply(4, 5), 20)
+        self.assertEqual(self.calc.multiply(-2, 3), -6)
+        self.assertEqual(self.calc.multiply(0, 100), 0)
+        self.assertEqual(self.calc.multiply(-4, -2), 8)
+
+    def test_divide(self):
+        self.assertEqual(self.calc.divide(10, 2), 5)
+        self.assertAlmostEqual(self.calc.divide(7, 3), 2.3333333, places=6)
+        self.assertEqual(self.calc.divide(-9, 3), -3)
+        self.assertEqual(self.calc.divide(-8, -2), 4)
+
+    def test_divide_by_zero(self):
+        with self.assertRaises(ZeroDivisionError):
+            self.calc.divide(10, 0)
+
+if __name__ == '__main__':
+    unittest.main()
